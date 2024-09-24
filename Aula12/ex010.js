3408637896c025b880a27032bd4a5478b48d0fc0
pablopmb <121886617+pablopmb@users.noreply.github.com>
2024-09-24 07:00:15 -0300

    ex10

---
+++ Aula12/ex010.js
@@ -0,0 +1,10 @@
+var idade = 70
+if ( idade < 16){
+    console.log(' Não pode votar')
+} else if (idade < 18) {
+    console.log('voto opicional')
+} else if ( idade < 70 ){
+    console.log('voto obrigatorio')
+} else if ( idade => 70){
+    console.log('voto opicional')
+}
