2d1af79b70544827cde83d896333b805162df93a
pablopmb <121886617+pablopmb@users.noreply.github.com>
2024-09-26 09:24:49 -0300

    repetição simples

---
+++ Aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+while( c <= 6){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1    ou seja  sempre vai somar o c mais 1
+}
+// Emquanto a variavel for menor ou igual a 6 vai acontecer o console.log('Tudo bem?'), 
+
+
+var c = 1
+do{
+    console.log(`p ${c}`)
+    c++
+}while( c <= 6)
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
---
+++ praticando/repeticao.js
@@ -0,0 +1,5 @@
+var p = 1
+do {
+    console.log(`pablo ${p}`)
+    p++
+}while(p <= 10)
