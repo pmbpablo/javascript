232bbb3e0204298856b752afeedfec2737cb7792
pablopmb <121886617+pablopmb@users.noreply.github.com>
2024-09-30 08:10:32 -0300

    TERMINEI O EX

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -6,20 +6,29 @@ function contar(){
 
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML= '[ERRO] Faltam dados!'
     }else{
         res.innerHTML = ('Contando...')
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-
-
-        for(var c = i; c <= f; c += p){
-
-            res.innerHTML += `${c}`
-
+        if(p <= 0){
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
         }
 
+        if (i < f){
+            // contagem crescente
+           for (var c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+            } 
+        } else {
+            // contagem regressiva
+            for (var c = i; c >= f ; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
 
     }
 }
