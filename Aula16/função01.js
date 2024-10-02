0e3a7560d22a7af5fe5718c50020d15a7006f941
pablopmb <121886617+pablopmb@users.noreply.github.com>
2024-10-02 09:44:19 -0300

    Aula16

---
+++ Aula16/função01.js
@@ -0,0 +1,10 @@
+function parimpar(n){
+    if (n%2 == 0){
+        return 'Par!'
+    }else {
+        return 'Inpar!'
+    }
+}
+//console.log(parimpar(10))
+var res = parimpar(8)
+console.log(res)
---
+++ Aula16/função02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0){ // esse =0 significa que caso não seja dado um valor ele automaticamente será 0
+    return n1 + n2
+}
+console.log(soma(7))
